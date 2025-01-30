0e77dbe0885b9b71a949c704083d20d14b91597d
roman <romanponchka@gmail.com>
2025-01-30 17:46:52 +0200

    interface for weather data added

---
+++ src/types/WeatherTypes.tsx
@@ -0,0 +1,31 @@
+interface ICondition{
+    text:string;
+    icon:string;
+}
+
+interface ICurrent{
+    cloud: number;
+    condition: ICondition;
+    feelslike_c: number;
+    gust_kph:number;
+    humidity:number
+    precip_mm:number;
+    temp_c:number;
+    wind_dir:string;
+    wind_kph:number
+}
+
+interface ILocation {
+    country: string;
+    lat:number;
+    localtime: string;
+    localtime_epoch: number;
+    lon:number;
+    name: string;
+    region: string;
+}
+
+export interface IWeatherData {
+    location:ILocation;
+    current:ICurrent;
+}
